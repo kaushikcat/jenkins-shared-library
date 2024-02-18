0e95683371356b1ea2b599b8851eb00acb77c7e6
KAUSHIK B V <101488279+kaushikcat@users.noreply.github.com>
2024-02-18 13:16:20 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,12 +1,12 @@
 def call() {
     // Define the AWS region and ECR repository URL
     def region = "ap-south-1"
-    def registry = "785236889276.dkr.ecr.ap-south-1.amazonaws.com/ecr3-ecr"
+    def registry = "670855725719.dkr.ecr.ap-south-1.amazonaws.com/kaushik-ecr"
 
 
 
    // Perform Docker image push to ECR
-    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'ecr3-ecr', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'kaushik-ecr', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
         sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${registry}"
         sh "docker push ${registry}"
     }
